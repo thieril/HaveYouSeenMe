afff2aed546c7429c3e2c5c7b5ad8dba604ef49f
thieril <thieril@dunwoody.edu>
2016-11-07 17:03:40 -0600

    Add project files.

---
+++ HaveYouSeenMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HaveYouSeenMe.Startup))]
+namespace HaveYouSeenMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
